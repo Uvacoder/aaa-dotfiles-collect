9311409c2b9d7e980c38caf4f8091e121d718570
Sergio Dobri <sergiodobri@gmail.com>
2021-07-02 19:16:57 -0300

    update tree config

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,10 +1,13 @@
-vim.g.nvim_tree_width = 50
+vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_git_hl = 0
+vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_ignore = {
   "dist",
   "plugin",
