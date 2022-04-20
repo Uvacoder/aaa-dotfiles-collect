8051815f6543ef1ac18edee2d455c7e02d3c9eb9
Sergio Dobri <sergiodobri@gmail.com>
2022-04-19 22:30:49 -0300

    add tokyo-night and remove night-fox

---
+++ nvim/lua/themes/tokyo-night.lua
@@ -0,0 +1,42 @@
+return {
+  setup = function(use)
+    use({
+      "folke/tokyonight.nvim",
+
+      config = function()
+        vim.g.tokyonight_style = "storm" -- storm, night
+        vim.g.tokyonight_terminal_colors = true
+        vim.g.tokyonight_italic_comments = true
+        vim.g.tokyonight_italic_keywords = false
+        vim.g.tokyonight_italic_functions = false
+        vim.g.tokyonight_italic_variables = false
+        vim.g.tokyonight_transparent = false
+        vim.g.tokyonight_transparent_sidebar = false
+        vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer", "NvimTree" }
+        vim.g.tokyonight_dark_sidebar = true
+        vim.g.tokyonight_dark_float = true
+        vim.g.tokyonight_hide_inactive_statusline = true
+
+        vim.g.tokyonight_colors = {
+          bg = vim.g.my_ui_colors.bg,
+          error = vim.g.my_colors.red,
+          warning = vim.g.my_colors.yellow,
+          info = vim.g.my_colors.blue,
+          hint = vim.g.my_colors.white,
+          gitSigns = {
+            delete = vim.g.my_colors.red,
+            change = vim.g.my_colors.yellow,
+            add = vim.g.my_colors.green,
+          },
+        }
+
+        -- Load the colorscheme
+        vim.cmd([[colorscheme tokyonight]])
+
+        vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = vim.g.my_ui_colors.bg })
+        vim.cmd([[highlight! link NeoTreeNormal NvimTreeNormal]])
+        vim.cmd([[highlight! link Terminal NvimTreeNormal]])
+      end,
+    })
+  end,
+}
