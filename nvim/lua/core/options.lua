487f0000ec7871ce96f7ff8baf88aa34ebab5a5b
Sergio Dobri <sergiodobri@gmail.com>
2022-04-23 01:23:09 -0300

    fix indent chars

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -9,7 +9,7 @@ vim.g.my_colors = {
 }
 vim.g.my_icons = {
   signs = { Error = " ", Warning = "𥉉", Hint = "ﬤ ", Info = " " },
-  diff = { add = "│", change = "│", delete = "│" },
+  diff = { add = "┃", change = "┃", delete = "┃" },
   modified = " ",
 }
 vim.g.my_border = "rounded" -- { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
--- nvim/lua/set/indent-blank-line.lua
+++ nvim/lua/set/indent-blank-line.lua
@@ -4,6 +4,8 @@ return {
       "lukas-reineke/indent-blankline.nvim",
       config = function()
         require("indent_blankline").setup({
+          char = " ",
+          indent_blankline_char_blankline = "│",
           show_current_context = true,
           show_current_context_start = false,
         })
